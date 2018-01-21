6f7a924eaf51ee2ce067b2a0d4dd7fcb2b5c9718
Kalimaha <guido.barbaglia@gmail.com>
2018-01-21 11:04:20 +1100

    Refactor

--- lib/rosetta_geotiff.ex
+++ lib/rosetta_geotiff.ex
@@ -1,17 +1,16 @@
 defmodule RosettaGeoTIFF do
   def read_headers(filename) do
     with {:ok, file} <- :file.open(filename, [:read, :binary]),
-         {:ok, header_bytes} <- header_bytes(file) do
-      order = order(header_bytes)
-      first_ifd = first_ifd(header_bytes, order)
-
-      {:ok, %{:order => order, :first_ifd => first_ifd}}
+         {:ok, header_bytes} <- header_bytes(file),
+         {:ok, endianess} <- endianess(header_bytes),
+         first_ifd <- first_ifd(header_bytes, endianess) do
+      {:ok, %{:endianess => endianess, :first_ifd => first_ifd}}
     else
       {:error, reason} -> {:error, format_error(filename, reason)}
     end
   end
 
-  defp header_bytes(file) do
+  def header_bytes(file) do
     :file.position(file, 0)
     bytes = :file.read(file, 8)
     :file.close(file)
@@ -19,22 +18,24 @@ defmodule RosettaGeoTIFF do
     bytes
   end
 
-  defp order(header_bytes) do
+  def endianess(header_bytes) do
     :binary.bin_to_list(header_bytes, {0, 2})
     |> :erlang.list_to_binary()
+    |> order2endianess
   end
 
-  defp first_ifd(header_bytes, order) do
-    case order do
-      "II" -> decode_first_ifd(header_bytes, :little)
-      "MM" -> decode_first_ifd(header_bytes, :big)
-    end
-  end
-
-  defp decode_first_ifd(header_bytes, endianness) do
+  def first_ifd(header_bytes, endianness) do
     :binary.bin_to_list(header_bytes, {4, 4})
     |> :erlang.list_to_binary()
     |> :binary.decode_unsigned(endianness)
+  end
+
+  defp order2endianess(order) do
+    case order do
+      "II" -> {:ok, :little}
+      "MM" -> {:ok, :big}
+      _ -> {:error, "Cannot determine endianess for '#{order}'."}
+    end
   end
 
   defp format_error(filename, reason) do
--- test/rosetta_geotiff_test.exs
+++ test/rosetta_geotiff_test.exs
@@ -8,7 +8,7 @@ defmodule RosettaGeoTIFFTest do
   test "opens the file", context do
     {:ok, headers} = RosettaGeoTIFF.read_headers(context[:filename])
 
-    assert headers[:order] == "II"
+    assert headers[:endianess] == :little
     assert headers[:first_ifd] == 270_276
   end
 
@@ -22,5 +22,40 @@ defmodule RosettaGeoTIFFTest do
 
       assert RosettaGeoTIFF.read_headers(context[:filename]) == {:error, msg}
     end
+  end
+
+  test "read first 8 bytes", context do
+    {:ok, file} = :file.open(context[:filename], [:read, :binary])
+    expected = {:ok, <<73, 73, 42, 0, 196, 31, 4, 0>>}
+    bytes = RosettaGeoTIFF.header_bytes(file)
+
+    assert bytes == expected
+  end
+
+  test "determines the endianess" do
+    ii_bytes = <<73, 73>>
+    mm_bytes = <<77, 77>>
+
+    assert RosettaGeoTIFF.endianess(ii_bytes) == {:ok, :little}
+    assert RosettaGeoTIFF.endianess(mm_bytes) == {:ok, :big}
+  end
+
+  test "when the endianess cannot be determined" do
+    bytes = <<105, 105>>
+    msg = "Cannot determine endianess for 'ii'."
+
+    assert RosettaGeoTIFF.endianess(bytes) == {:error, msg}
+  end
+
+  test "finds the first IFD for II files" do
+    header_bytes = <<0, 0, 0, 0, 0, 0, 0, 42>>
+
+    assert RosettaGeoTIFF.first_ifd(header_bytes, :little) == 704_643_072
+  end
+
+  test "finds the first IFD for MM files" do
+    header_bytes = <<0, 0, 0, 0, 42, 0, 0, 0>>
+
+    assert RosettaGeoTIFF.first_ifd(header_bytes, :big) == 704_643_072
   end
 end
